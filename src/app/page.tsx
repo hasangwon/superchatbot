b3a6f7c7c09fef2826a554acbce2d78562c599fc
hasangwon <305243@naver.com>
2025-03-25 18:18:36 +0900

    Test: 공유하기 버튼 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,60 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import React, { useEffect } from "react";
+import TextareaAutosize from "react-textarea-autosize";
+
+const Home = () => {
+  const [text, setText] = React.useState("");
+
+  useEffect(() => {
+    setText(
+      "안녕하세요. 이건 공유하기 테스트입니다. 이 텍스트를 공유하려면 아래의 버튼을 클릭해주세요."
+    );
+  }, []);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="bg-white w-full h-full min-w-[5rem] overflow-hidden">
+      <main className="w-full h-full flex flex-col overflow-hidden pb-[5rem]">
+        <header className="h-20 border-b flex justify-center items-center">
+          챗봇
+        </header>
+        <section className="h-full basis-[100%] flex flex-col overflow-y-auto overflow-x-hidden p-4">
+          <div className="flex items-center mb-2">
+            <div className="w-6 h-6 bg-gray-100 rounded-full border mr-1" />
+            키움증권 챗봇
+          </div>
+          <div className="p-5 bg-white border rounded-2xl">
+            안녕하세요. 이건 공유하기 테스트입니다. 이 텍스트를 공유하려면
+            아래의 버튼을 클릭해주세요.
+            <button
+              className="border p-4 w-full"
+              onClick={() => {
+                if (navigator.share) {
+                  navigator
+                    .share({
+                      title: "웹 쉐어 API",
+                      text: text,
+                    })
+                    .then(() => console.log("Successful share"))
+                    .catch((error) => console.log("Error sharing", error));
+                }
+              }}
+            >
+              공유하기
+            </button>
+          </div>
+        </section>
+        <div className="fixed bottom-0 h-[5rem] bg-white w-full border-t flex items-center p-4">
+          <TextareaAutosize
+            className="border w-full"
+            minRows={1}
+            maxRows={2}
+            placeholder="내용을 입력하세요..."
+          />
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+      <></>
     </div>
   );
-}
+};
+export default Home;
